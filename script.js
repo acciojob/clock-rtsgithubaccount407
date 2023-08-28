cfbb094e3200fed23e47d90f647b332575f195c1
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-08-28 16:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function livetime() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+  let cudate = `${month}/${day}/${year}`;
+  document.getElementById('timer').textContent = cudate + ', ' + time;
+}
+livetime()
+function startTime(){
+	setInterval(livetime,1000)
+}
+window.addEventListener('load' ,startTimer)
